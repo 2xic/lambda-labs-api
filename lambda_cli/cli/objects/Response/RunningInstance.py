34142105939169034ada6ef6a6a342fe8239a724
2xic <5640782+2xic@users.noreply.github.com>
2023-02-01 20:30:51 +0000

    Handle booting status

--- lambda_cli/cli/objects/Response/RunningInstance.py
+++ lambda_cli/cli/objects/Response/RunningInstance.py
@@ -4,7 +4,7 @@ class RunningInstance:
     def __init__(self, json_object) -> None:
         self.id = json_object["id"]
         self.status = json_object["status"]
-        self.ip = json_object["ip"]
+        self.ip = json_object.get("ip", None)
         self.instance = BaseInstance(json_object["instance_type"])
         self.jupyter_url = json_object.get("jupyter_url", None)
 
@@ -12,4 +12,6 @@ class RunningInstance:
         return self.__str__()
 
     def __str__(self):
-        return f"{self.instance.name} {self.ip} ({self.status}) {self.instance.price_dollar}$ per hour"
+        if self.status != 'booting':
+            return f"{self.instance.name} {self.ip} ({self.status}) {self.instance.price_dollar}$ per hour"
+        return f"{self.instance.name} ({self.status}) {self.instance.price_dollar}$ per hour"
